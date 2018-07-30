43bc37289c9512ffe5b5da8b57873ceac7c227ce
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-30 15:07:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Monica"
+  end
+
+  get '/hometown' do
+    "My hometown is Los Angeles"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is all the songs, jk (but a lot of them)"
+  end
+
 end
